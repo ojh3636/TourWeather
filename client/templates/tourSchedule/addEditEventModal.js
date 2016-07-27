c4bbf351b1cee1b9bcc4efade1f7899dd24e5e67
JongHun Oh <ojh3636@kaist.ac.kr>
2016-07-27 22:32:47 +0900

    add modal

---
+++ client/templates/tourSchedule/addEditEventModal.js
@@ -0,0 +1,63 @@
+let closeModal = () => {
+  $( '#add-edit-event-modal' ).modal( 'hide' );
+  $( '.modal-backdrop' ).fadeOut();
+};
+
+Template.addEditEventModal.helpers({
+  modalType( type ) {
+    let eventModal = Session.get( 'eventModal' );
+    if ( eventModal ) {
+      return eventModal.type === type;
+    }
+  },
+  modalLabel() {
+    let eventModal = Session.get( 'eventModal' );
+
+    if ( eventModal ) {
+      return {
+        button: eventModal.type === 'edit' ? 'Edit' : 'Add',
+        label: eventModal.type === 'edit' ? 'Edit' : 'Add an'
+      };
+    }
+  },
+  selected( v1, v2 ) {
+    return v1 === v2;
+  },
+  event() {
+    let eventModal = Session.get( 'eventModal' );
+
+    if ( eventModal ) {
+      return eventModal.type === 'edit' ? Events.findOne( eventModal.event ) : {
+        start: eventModal.date,
+        end: eventModal.date
+      };
+    }
+  }
+});
+
+Template.addEditEventModal.events({
+  'submit form' ( event, template ) {
+    event.preventDefault();
+
+    let eventModal = Session.get( 'eventModal' ),
+        submitType = eventModal.type === 'edit' ? 'editEvent' : 'addEvent',
+        eventItem  = {
+          place: template.find( '[name="place"]' ).value,
+          from: template.find( '[name="start"]' ).value,
+          end: template.find( '[name="end"]' ).value,
+        };
+
+    if ( submitType === 'editEvent' ) {
+      eventItem._id   = eventModal.event;
+    }
+
+    Meteor.call( submitType, eventItem, ( error ) => {
+      if ( error ) {
+        Bert.alert( error.reason, 'danger' );
+      } else {
+        Bert.alert( `Event ${ eventModal.type }ed!`, 'success' );
+        closeModal();
+      }
+    });
+  }
+});
--- client/templates/tourSchedule/scheduleSubmit.js
+++ client/templates/tourSchedule/scheduleSubmit.js
@@ -1,3 +1,8 @@
+let isPast = (date) => {
+  let today = moment().format();
+  return moment(today).isAfter(date);
+};
+
 Template.scheduleSubmit.onRendered(function() {
   console.log("Rendered");
   this.autorun(function() {
@@ -9,6 +14,35 @@ Template.scheduleSubmit.onRendered(function() {
     }
   });
 
-  $('#calendar').fullCalendar();
+  $('#calendar').fullCalendar({
+    events(start, end, timezone, callback) {
+      let data = SubTrips.find().fetch().map(function(event) {
+        event.editable = !isPast(event.from);
+        event.start = event.from;
+        event.title = event.place;
+        return event;
+      });
+
+
+      if (data) {
+        callback(data);
+      }
+    },
+    dayClick(date){
+
+      Session.set('eventModal', {type:'add',date: date.format()});
+      $('#add-edit-event-modal').modal('show');
+    },
+    eventClick(event) {
+      Session.set('eventModal', {type: 'edit', event: event._id});
+      $('#add-edit-event-modal').modal('show');
+    }
+  });
+
+
+  Tracker.autorun(function() {
+    SubTrips.find().fetch();
+    $('#calendar').fullCalendar('refetchEvents');
+  });
 
 });
